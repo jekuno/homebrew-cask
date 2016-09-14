63552bcbd5111479edb1c7fb93e2f34d88858af1
jekuno <jekuno@users.noreply.github.com>
2016-09-14 20:58:09 +0200

    Updates darktable to version 2.0.6

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,11 +1,11 @@
 cask 'darktable' do
-  version '2.0.5'
-  sha256 'e0ae0e5e19771810a80d6851e022ad5e51fb7da75dcbb98d96ab5120b38955fd'
+  version '2.0.6'
+  sha256 '2368c1865221032061645342ba8c00bcd6d224e9829a55bc610e6cb67de738c1'
 
   # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
   appcast 'https://github.com/darktable-org/darktable/releases.atom',
-          checkpoint: '097513cfe481beb354fc63002a3255646cbf5c5b2e23b9f6e60d58e9a7c5b45d'
+          checkpoint: '36d7b67fcf9248d6ee99acd3fe440f462486d66c534ee51ddaa82d49cd82fdef'
   name 'darktable'
   homepage 'https://www.darktable.org/'
   license :gpl
